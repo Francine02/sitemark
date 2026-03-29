826b823d62e0a7c490e92b7eb6f002cb2f84fbfe
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-03-29 16:58:31 -0300

    feat(models): add fields to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,19 +3,28 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'surname',
+        'email',
+        'password',
+        'bio',
+        'image'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token'
+    ];
 
     /**
      * Get the attributes that should be cast.
@@ -28,5 +37,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function links()
+    {
+        return $this->hasMany(Link::class);
     }
 }
