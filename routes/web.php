1703f69ee10e62e13e7c273213c3c6623ff99a91
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-03-29 17:01:21 -0300

    feat(routes): add register and login routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('auth')->group(function () {
+    Route::get('/register', [UserController::class, 'showRegisterForm'])->name('register');
+    Route::get('/register', [UserController::class, 'store'])->name('register');
+
+    Route::get('/login', [UserController::class, 'showLoginForm'])->name('login');
+    Route::get('/login', [UserController::class, 'login'])->name('login');
 });
