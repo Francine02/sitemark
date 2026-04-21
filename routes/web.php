4dec21ef5dcc14b93859654517e6ebc43cf819f3
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-21 14:44:43 -0300

    feat(routes): add home, profile and logout routes.  Change methods of rouutes login and register

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,30 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.login');
 });
 
 Route::prefix('auth')->group(function () {
-    Route::get('/register', [UserController::class, 'showRegisterForm'])->name('register');
-    Route::get('/register', [UserController::class, 'store'])->name('register');
+    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+    Route::post('/register', [AuthController::class, 'store'])->name('register');
 
-    Route::get('/login', [UserController::class, 'showLoginForm'])->name('login');
-    Route::get('/login', [UserController::class, 'login'])->name('login');
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/home', function () {
+        return view('pages.home.index');
+    })->name('home');
+
+    Route::prefix('/profile')->group(function () {
+        Route::get('/', [UserController::class, 'showProfile'])->name('profile');
+        Route::put('/update', [UserController::class, 'update'])->name('profile.update');
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
