1092ddc8afcca94bdb5d58eae6e8e4de46989bf4
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-21 14:45:33 -0300

    feat(pages): add home page

---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.content')
+
+@section('content-dashboard')
+    <div class="flex justify-between items-center mb-17">
+        <x-title>
+            Links
+        </x-title>
+
+        <x-button variant="dark" class="items-center flex gap-2">
+            <i class="iconoir-plus-circle text-lg"></i> Adicionar link
+        </x-button>
+    </div>
+
+    <div>
+        
+    </div>
+@endsection
