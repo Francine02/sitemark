245d8f1c03c492a5119449583fd4f6b6cab93d93
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-21 14:43:16 -0300

    feat(views): add Toastify cdn and script for use toastify

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,6 +9,10 @@
 
     <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
 
+    <!-- Toastify -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+
     <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/iconoir-icons/iconoir@main/css/iconoir.css" />
@@ -16,6 +20,46 @@
 
 <body class="bg-background-primary font-main">
     @yield('content')
+
+    @if (session('success'))
+        <script>
+            Toastify({
+                text: `
+                <div style="display:flex; align-items:center; gap:8px;">
+                    <i class="iconoir-check-circle-solid text-accent-green"></i>
+                    <span>{{ session('success') }}</span>
+                </div>
+            `,
+                duration: 3000,
+                gravity: "top",
+                position: "right",
+                escapeMarkup: false,
+                style: {
+                    background: "#110f0e"
+                }
+            }).showToast();
+        </script>
+    @endif
+
+    @if (session('error'))
+        <script>
+            Toastify({
+                text: `
+                <div style="display:flex; align-items:center; gap:8px;">
+                    <i class="iconoir-xmark-circle-solid text-accent-red"></i>
+                    <span>{{ session('error') }}</span>
+                </div>
+            `,
+                duration: 3000,
+                gravity: "top",
+                position: "right",
+                escapeMarkup: false,
+                style: {
+                    background: "#110f0e"
+                }
+            }).showToast();
+        </script>
+    @endif
 </body>
 
 </html>
