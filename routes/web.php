db33b3e14d9182e930e246bf41f17d1e1c155e65
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-26 14:29:15 -0300

    feat: add links route and change home route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LinkController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,13 +18,18 @@ Route::prefix('auth')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/home', function () {
-        return view('pages.home.index');
-    })->name('home');
+    Route::get('/home', [LinkController::class, 'index'])->name('home');
 
     Route::prefix('/profile')->group(function () {
         Route::get('/', [UserController::class, 'showProfile'])->name('profile');
         Route::put('/update', [UserController::class, 'update'])->name('profile.update');
+    });
+
+    Route::prefix('/links')->group(function () {
+        Route::post('/store', [LinkController::class, 'store'])->name('links.store');
+        Route::put('/update/{id}', [LinkController::class, 'update'])->name('links.update');
+        Route::put('/reorder', [LinkController::class, 'updatePosition'])->name('links.reorder');
+        Route::delete('/delete/{id}', [LinkController::class, 'destroy'])->name('links.delete');
     });
 
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
