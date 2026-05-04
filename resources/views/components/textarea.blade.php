82cabcd3135715905ff54962afe4f3a288b88395
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-04 18:40:38 -0300

    feat(components): add old value with data_get in Textarea

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -12,11 +12,11 @@
 <div>
     <label class="paragraph-medium block mt-5 mb-2">
         {{ $label }}
-        <textarea rows="5" name="{{ $name }}" value="{{ old($name) }}" type="{{ $type }}"
+        <textarea rows="5" name="{{ $name }}" type="{{ $type }}"
             placeholder="{{ $placeholder }}" @class([
                 'bg-background-secondary w-full p-3 rounded-xl outline-0 hover:bg-background-tertiary/60 placeholder:text-content-tertiary mt-1 min-h-24 max-h-40',
                 'border-accent-red border' => $hasError,
-            ])></textarea>
+            ])>{{ old($name, data_get(auth()->user(), $name)) }}</textarea>
     </label>
 
     @if ($errors->has($name))
