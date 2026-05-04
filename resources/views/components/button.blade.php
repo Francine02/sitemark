7a6b2cc846d5a6c0d9994b21b3838b4a6a6a20df
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-04 18:37:15 -0300

    feat(components): add attributes merte to Button component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,19 +1,26 @@
 @props([
     'variant' => 'default',
     'type' => 'button',
-    'class' => ''
+    'class' => '',
 ])
 
-@php 
+@php
     $variants = [
         'default' => 'px-8 py-3 text-content-inverse bg-accent-orange rounded-4xl font-semibold',
-        'dark' => 'px-8 py-3.5 bg-background-secondary text-accent-orange rounded-4xl font-semibold'
+        'dark' => 'px-8 py-3.5 bg-background-secondary text-accent-orange rounded-4xl font-semibold',
     ];
 
     $classes = $variants[$variant] ?? $variants['default'];
 
 @endphp
 
-<button type="{{ $type }}" class="hover:cursor-pointer hover:brightness-90 {{ $classes }} {{ $class }}">
-    {{ $slot }}
+<button type="{{ $type }}"
+    {{ $attributes->merge([
+        'class' => "hover:cursor-pointer hover:brightness-90 disabled:cursor-not-allowed disabled:hover:brightness-100 $classes $class",
+    ]) }}>
+    <span class="btn-text items-center flex gap-2">
+        {{ $slot }}
+    </span>
+
+    <x-loading />
 </button>
