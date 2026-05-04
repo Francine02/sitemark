12bf1ceac01b2415cdb221f03c9f3ab4b7cc8c39
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-04 18:42:33 -0300

    feat: add event when doc loaded

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { loading } from "./loading";
+import { openModal } from "./modal";
+import { sortable } from "./sortable";
+
+document.addEventListener("DOMContentLoaded", () => {
+    sortable();
+    openModal();
+    loading();
+});
