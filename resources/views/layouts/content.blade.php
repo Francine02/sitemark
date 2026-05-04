f67d8d15b494670d72c13364025236a51fe3be8d
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-04 18:41:29 -0300

    feat(components): add meta csrf-token in content layout

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.default')
 
 @section('content')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <main class="p-5 lg:min-h-screen 2xl:max-w-7xl 2xl:mx-auto gap-10 relative mb-40 lg:mb-0">
-        <img onclick="window.location.href='{{ route('home') }}'" class="mx-auto mb-16 hover:cursor-pointer" src="/images/logo.png" alt="sitemark" />
+        <img onclick="window.location.href='{{ route('home') }}'" class="mx-auto mb-16 hover:cursor-pointer"
+            src="/images/logo.png" alt="sitemark" />
 
-        <section class="text-content-primary m-auto lg:max-w-3xl w-full">
+        <section class="text-content-primary m-auto md:max-w-3xl w-full">
             @yield('content-dashboard')
-
         </section>
 
-        <x-nav-bar/>
+        <x-nav-bar />
     </main>
 @endsection
