e081bb603d2dc2b938139552e01937e55e3b4dfb
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-04 18:41:57 -0300

    feat(components): add link-form, form and cards to Home page

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -6,12 +6,23 @@
             Links
         </x-title>
 
-        <x-button variant="dark" class="items-center flex gap-2">
+        <x-button type="button" class="open-modal-btn" data-action="create" variant="dark">
             <i class="iconoir-plus-circle text-lg"></i> Adicionar link
         </x-button>
     </div>
 
-    <div>
-        
+    <x-modal>
+        <x-link-form />
+    </x-modal>
+
+    <form id="reorder-form" method="POST" action="{{ route('links.reorder') }}">
+        @csrf
+        @method('PUT')
+        <input type="hidden" name="order" id="order-input">
+    </form>
+    <div id="links-list" class="space-y-4 max-h-[50svh] overflow-y-auto px-5 md:px-10 z-0">
+        @foreach ($links as $link)
+            <x-card :link="$link" />
+        @endforeach
     </div>
 @endsection
