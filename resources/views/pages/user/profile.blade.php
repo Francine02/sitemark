97af2d407908dda3259cdcbc4c91286fb280d7e2
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-04 18:42:13 -0300

    feat(components): add id in Profile page

--- resources/views/pages/user/profile.blade.php
+++ resources/views/pages/user/profile.blade.php
@@ -4,20 +4,19 @@
     <div class="flex justify-between items-center mb-17 lg:mb-0">
         <x-title>Perfil</x-title>
 
-        <x-button type="submit" class="w-30">Salvar</x-button>
+        <x-button form="profile-form" type="submit" class="w-30">Salvar</x-button>
     </div>
 
-    <form class="flex flex-col lg:flex-row-reverse gap-10" action="{{ route('profile.update') }}" method="POST"
-        enctype="multipart/form-data">
+    <form id="profile-form" class="flex flex-col lg:flex-row-reverse gap-10" action="{{ route('profile.update') }}"
+        method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
 
-        <x-input-image />
+        <x-input-image
+            src="{{ auth()->user()->image ? asset('storage/' . auth()->user()->image) : asset('images/preview-default.png') }}" />
 
         <div class="w-full">
             <x-input name="name" placeholder="Maria" label="Nome" />
-
-            <x-input name="surname" placeholder="Hermisstton" label="Sobrenome" />
 
             <x-input placeholder="maria_hermiston@gmail.com" />
 
